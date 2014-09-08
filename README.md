3eccb8828cf3dd43b2af7f12297cddacffb13ffe
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-09-07 20:34:00 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,25 @@
+# garble
+A fun JavaScript text-garbler using Unicode combining characters (diacritics).
+
+[![Latest Stable Version](http://img.shields.io/npm/v/garble.svg?style=flat)](https://www.npmjs.org/package/garble)
+[![Total Downloads](http://img.shields.io/npm/dm/garble.svg?style=flat)](https://www.npmjs.org/package/garble)
+[![License](http://img.shields.io/npm/l/garble.svg?style=flat)](https://www.npmjs.org/package/garble)
+
+## Usage
+This library exports a function (named `garble` for browser users) that garbles
+text in a random fashion using Unicode combining characters (diacritics).  For
+example:
+```js
+var garble = require("garble");
+
+console.log(garble("hello, world"));
+// h͚̠̙̒e̖͇ͬ̕ļ̹̯ͩl̘ͮ͢͞o̷̪͓ͅ,͚̑̊͢ ̤̫̿͌w̥̦̹͢ö̥͊͜r̜̓̅͘l̓̉̀͝d̫́̉
+```
+
+It takes additional parameters to control the minimum and maximum number of diacritics added to each character (defaults to 20-30):
+```js
+var garble = require("garble");
+
+console.log(garble("hello, world", 0, 1));
+// ḧe̟l͕l̝o, w̅orl̢d̅
+```
---
+++ garble.js
@@ -0,0 +1,60 @@
+(function() {
+  "use strict";
+
+  // Helper function to return an array of all of the characters between two
+  // characters inclusive.
+  function charRange(low, high) {
+    var result = [];
+    for (var current = low.charCodeAt(0); current <= high.charCodeAt(0); current++) {
+      result.push(String.fromCharCode(current));
+    }
+
+    return result;
+  }
+
+  // A list of the acceptable diactric characters.  Other UTF-8 Combining
+  // Character ranges aren't well supported.
+  var diacritics = charRange("\u0300", "\u036f");
+
+  // A helper function that returns a random diactric from the list.
+  function randomDiacritic() {
+    return diacritics[Math.floor(Math.random() * diacritics.length)];
+  }
+
+  // "Garbles" text by adding Unicode Combining Characters (diacritics) to
+  // every character.  Defaults to adding 20-30 diacritics to each character.
+  var garble = function(str, minDiacritics, maxDiacritics) {
+    var garbled = "";
+
+    minDiacritics = typeof minDiacritics === "undefined" ? 20 : minDiacritics;
+    maxDiacritics = typeof maxDiacritics === "undefined" ? 30 : maxDiacritics;
+    var diactricRange = maxDiacritics - minDiacritics + 1;
+
+    for (var i = 0; i < str.length; i++) {
+      garbled += str.charAt(i);
+      var numDiactritics = Math.floor(Math.random() * diactricRange) + minDiacritics;
+      for (var j = 0; j < numDiactritics; j++) {
+        garbled += randomDiacritic();
+      }
+    }
+
+    return garbled;
+  };
+
+  // Handle node.js and browser includes.
+  if (typeof exports !== "undefined") {
+    if (typeof module !== "undefined" && module.exports) {
+      exports = module.exports = garble;
+    }
+    exports.garble = garble;
+  } else {
+    this.garble = garble;
+  }
+
+  // Handle AMD autoloaders.
+  if (typeof define === "function" && define.amd) {
+    define("garble", [], function() {
+      return garble;
+    });
+  }
+}.call(this));
